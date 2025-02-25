0dfa075ecc2a55f66efab4e5ed68988da432d9c5
vinhco1 <23520189@gm.uit.edu.vn>
2025-02-25 16:29:05 +0700

    Refactor Project Structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
+import App from "./App";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
       <App />
+      <CssBaseline />
     </ThemeProvider>
   </StrictMode>
 );
