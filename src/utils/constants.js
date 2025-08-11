5239a702c1d3737519151fe4268814be2e8785b4
Vinh Co <23520189@gm.uit.edu.vn>
2025-08-11 13:13:44 +0000

    chore: Update package.json and constants for environment configuration and add cross-env dependency

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,11 @@
-export const API_ROOT = "http://localhost:3000";
+/* eslint-disable no-undef */
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:3000'
+} else if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-web-ten-nu.vercel.app'
+}
+
+
+export const API_ROOT = apiRoot;
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    // eslint-disable-next-line no-undef
+    "process.env": process.env
+  },
   plugins: [
     react(),
     svgr()
